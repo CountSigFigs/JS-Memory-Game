83f9b794ef0d4c47e3b996c6f7e299828315c616
Brian Holt <bharrisonholt@gmail.com>
2020-04-23 08:55:04 -0700

    refactoring

--- script.js
+++ script.js
@@ -8,25 +8,35 @@ function flipCard(){
    this.classList.add('flip')
 
    if (!hasFlippedCard){
+      //first click
       hasFlippedCard=true;
       firstCard=this;
-   } else {
+      return;
+   } 
+      //second click
       hasFlippedCard= false;
       secondCard= this
-
-     if (firstCard.dataset.framework === secondCard.dataset.framework){
-        firstCard.removeEventListener('click',flipCard)
-        secondCard.removeEventListener('click', flipCard)
-     } else {
-        setTimeout(()=>{
-         firstCard.classList.remove('flip')
-         secondCard.classList.remove('flip')
-        }, 1500)
-     }
-   }
+      checkForMatch()
 }
 
+function checkForMatch(){
+   let isMatch= firstCard.dataset.framework === secondCard.dataset.framework
 
+   isMatch ?  disableCards() : unflipCards()
+   
+}
+
+function disableCards(){
+   firstCard.removeEventListener('click',flipCard)
+   secondCard.removeEventListener('click', flipCard)
+}
+
+function unflipCards (){
+   setTimeout(()=>{
+      firstCard.classList.remove('flip')
+      secondCard.classList.remove('flip')
+     }, 1500)
+}
 
 cards.forEach(card => card.addEventListener('click', flipCard));
 
