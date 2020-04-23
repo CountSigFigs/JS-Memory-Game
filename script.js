a6858be7709b8482c7cca58656e1f48a91b49720
Brian Holt <bharrisonholt@gmail.com>
2020-04-22 17:34:10 -0700

    setting up match

--- script.js
+++ script.js
@@ -1,9 +1,33 @@
 
 const cards= document.querySelectorAll('.memory-card');
 
+let hasFlippedCard = false;
+let firstCard, secondCard;
+
 function flipCard (){
    this.classList.toggle('flip')
-   console.log(this.classList)
+
+   if (!hasFlippedCard){
+      //first click
+      hasFlippedCard=true;
+      firstCard= this;
+   } else {
+      //second click
+      hasFlippedCard= false;
+      secondCard=this
+
+      //do cards match?
+      if (firstCard.dataset.framework === secondCard.dataset.framework){
+         firstCard.removeEventListener('click',flipCard)
+         secondCard.removeEventListener('click',flipCard)
+      } else {
+         setTimeout(()=>{
+            firstCard.classList.remove('flip')
+            secondCard.classList.remove('flip')
+         }, 1500)
+         
+      }
+   }
 }
 
 cards.forEach(card => card.addEventListener('click', flipCard));
