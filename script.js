a1cfa9e51a0a5720379b2a5dd8fdc4ec1fcf1ac4
Brian Holt <bharrisonholt@gmail.com>
2020-04-22 19:27:49 -0700

    going back to fix bug

--- script.js
+++ script.js
@@ -1,48 +1,32 @@
 
 const cards= document.querySelectorAll('.memory-card');
-let lockBoard= false;
-let hasFlippedCard = false;
+let hasFlippedCard= false;
 let firstCard, secondCard;
 
-function flipCard (){
-   if (lockBoard) return;
-   if (this === firstCard) return;
-   this.classList.toggle('flip')
+
+function flipCard(){
+   this.classList.add('flip')
 
    if (!hasFlippedCard){
-      //first click
       hasFlippedCard=true;
-      firstCard= this;
-
-      return;
-   }
-      //second click
+      firstCard=this;
+   } else {
       hasFlippedCard= false;
-      secondCard=this
+      secondCard= this
 
-      checkForMatch()
+     if (firstCard.dataset.framework === secondCard.dataset.framework){
+        firstCard.removeEventListener('click',flipCard)
+        secondCard.removeEventListener('click', flipCard)
+     } else {
+        setTimeout(()=>{
+         firstCard.classList.remove('flip')
+         secondCard.classList.remove('flip')
+        }, 1500)
+     }
+   }
 }
 
-function checkForMatch (){
 
-   let isMatch= firstCard.dataset.framework === secondCard.dataset.framework;
-   isMatch ? disableCards() : unflipCards();
-  
-}
-
-function disableCards(){
-   firstCard.removeEventListener('click',flipCard)
-   secondCard.removeEventListener('click',flipCard)
-}
-
-function unflipCards(){
-   lockBoard=true;
-   setTimeout(()=>{
-      firstCard.classList.remove('flip')
-      secondCard.classList.remove('flip')
-      lockBoard=false;
-   }, 1500)
-}
 
 cards.forEach(card => card.addEventListener('click', flipCard));
 
